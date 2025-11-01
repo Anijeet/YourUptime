74ef3719027907bb4f26bee84e843bb1cdcdce91
Anijeet mani <anijeet@razorbooking.com>
2025-11-01 11:09:22 +0530

    updated with endpoints with serde

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,13 +1,23 @@
-use poem::{IntoResponse, Route, Server, get, post, handler, listener::TcpListener, web::Path};
+use poem::{ Route, Server, get, handler, listener::TcpListener, post, web::{Json, Path}};
+
+use crate::{request_input::CreateWebsiteInput, request_output::CreateWebsiteOutput};
+
+pub mod request_input;
+pub mod request_output;
 
 #[handler]
-fn create_website(Path(website_id): Path<String>) -> String {
-    format!("hello: {}", website_id)
+fn create_website(Json(data): Json<CreateWebsiteInput>) -> Json<CreateWebsiteOutput>{
+    // let url = data.url;
+    let response = CreateWebsiteOutput {
+        id: data.url,
+    };
+
+    Json(response)
 }
 
 #[handler]
 fn get_website(Path(website_id): Path<String>) -> String {
-    format!("hello: {}", website_id)
+    format!("hello: {}", website_id) //format concatenates strings like `hello ${website_id}`
 }
 
 #[tokio::main]
@@ -16,6 +26,7 @@ async fn main() -> Result<(), std::io::Error> {
     .at("/status/:website_id", get(create_website))
     .at("/website", post(get_website));
 
+    //create a http  server that listens on port 3001
     Server::new(TcpListener::bind("0.0.0.0:3001"))
         .run(app)
         .await
